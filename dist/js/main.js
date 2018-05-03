c314c84ed9bcda0e25fa2ddd09a1a13be21fe241
Nataly <ncgarzon88@gmail.com>
2018-05-03 00:06:54 -0400

    IT WORKS - Commit - needs API
    The api needs updating from GREG

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,5 @@
+// console.log(`Hello World from app.js! 
+// Change this message, and make sure it changes in the browser 
+// to verify that you're working in the right files.`)
 "use strict";
-
-console.log("Hello World from app.js! \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files.");
 //# sourceMappingURL=main.js.map
